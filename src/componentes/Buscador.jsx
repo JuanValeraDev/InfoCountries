b8f0d81f5047d6576d4ee2c82cb79c99efc1cbe5
JuanValeraDev <juanvaleradev@gmail.com>
2023-12-18 12:15:09 +0100

    He avanzado un poco en el componente Buscador. Ya se ve la imagen de fondo y ocupa el tamaño correcto

--- src/componentes/Buscador.jsx
+++ src/componentes/Buscador.jsx
@@ -4,9 +4,7 @@ import {Fondo} from "./Fondo.jsx";
 import fondo_buscador from "../assets/fondo_buscador 16.32.40.jpeg"
 
 export const Buscador = () => {
-    return (<div>
-        <Fondo imagen={fondo_buscador}>
-        <Rectangulo backgroundColor={1} textColor={1} borderColor={1}/>
-        </Fondo>
+    return (<div className={"buscador"}>
+        <Fondo imagen={fondo_buscador}/>
     </div>)
 }

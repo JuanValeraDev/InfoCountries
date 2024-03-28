abc00f177695ef9c94a2a2c6b689a050c412cb68
Victor <elsodah12@gmail.com>
2024-03-28 15:47:02 +0100

    He agregado el about al footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
-import {Introduccion} from './componentes/Introduccion'
-import {Opciones} from './componentes/Opciones'
-import {Buscador} from "./componentes/Buscador.jsx";
-import {Galeria} from "./componentes/Galeria.jsx";
-import {Juego} from "./componentes/Juego.jsx";
-import {About} from "./componentes/About.jsx";
-import {Comparador} from './componentes/Comparador.jsx';
-import {BrowserRouter} from 'react-router-dom';
-import {Routes, Route, Outlet, Link as Router} from "react-router-dom";
-import {Pagenotfound} from './componentes/Pagenotfound.jsx';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Introduccion } from './componentes/Introduccion'
+import { Opciones } from './componentes/Opciones'
+import { Buscador } from "./componentes/Buscador.jsx";
+import { Galeria } from "./componentes/Galeria.jsx";
+import { Juego } from "./componentes/Juego.jsx";
+import { About } from "./componentes/About.jsx";
+import { Comparador } from './componentes/Comparador.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Outlet, Link as Router } from "react-router-dom";
+import { Pagenotfound } from './componentes/Pagenotfound.jsx';
 import Footer from "./componentes/Footer.jsx";
 
 function App() {
@@ -25,18 +25,21 @@ function App() {
             {<About />} */}
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<> <Introduccion/>
-                        <Opciones/></>}/>
+                    <Route index element={
+                        <>
+                            <Introduccion />
+                            <Opciones />
+                            <Footer />
+                        </>} />
                     {/*} <Route path="/opciones" element={<Opciones/>}/>*/}
-                    <Route path="/busquedaAvanzada" element={<Buscador/>}/>
-                    <Route path="/comparador" element={<Comparador/>}/>
-                    <Route path="/galeria" element={<Galeria/>}/>
-                    <Route path="/minijuego" element={<Juego/>}/>
-                    <Route path="*" element={<Pagenotfound/>}/>
+                    <Route path="/busquedaAvanzada" element={<Buscador />} />
+                    <Route path="/comparador" element={<Comparador />} />
+                    <Route path="/galeria" element={<Galeria />} />
+                    <Route path="/minijuego" element={<Juego />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<Pagenotfound />} />
                 </Routes>
             </BrowserRouter>
-            <Footer/>
-
         </>
     )
 }
--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Col, Row} from "react-bootstrap";
-import {TextareaAutosize} from "@mui/material";
-
+import { useNavigate } from 'react-router-dom';
+ 
 const Footer = () => {
+    const navigate = useNavigate()
     return (
         <footer style={{
             position: 'fixed',
@@ -16,7 +17,7 @@ const Footer = () => {
         }}>
             <Row className={"d-flex row"}>
                 <Col className={"align-self-center"}>InfoCountries</Col>
-                <Col>Juan Valera y Víctor Gálvez</Col>
+                <Col>Juan Valera y Víctor Gálvez <u onClick={() => navigate("about")} className='mx-2 about'>about</u></Col>
             </Row>
         </footer>
     );

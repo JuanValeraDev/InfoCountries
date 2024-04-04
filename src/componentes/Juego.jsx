0d3bd246708163cf0c58b989bfae8e84141a50bf
Victor <elsodah12@gmail.com>
2024-04-04 19:41:51 +0200

    Ya he arreglado lo del minijuego. Funciona perfe, ahora solo queda hacer los eventos, el modo de juego, una puntuacion y ya hacerlo todo responsive etc etc etc

--- src/componentes/Juego.jsx
+++ src/componentes/Juego.jsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from "react"
-import { Col, Row } from "react-bootstrap";
-import shape_4 from "../assets/shapes/shape_4_modified.png"
 import { Rectangulo } from "./Rectangulo.jsx";
 
 
 export const Juego = () => {
-
-    const [countries, setCountries] = useState([]);
     const [correctCountry, setCorrectCountry] = useState({});
     const [options, setOptions] = useState([]);
 
@@ -16,16 +12,17 @@ export const Juego = () => {
                 const flagsApiResponse = await fetch('https://restcountries.com/v3.1/all');
                 const countriesData = await flagsApiResponse.json();
 
-                const randomIndex = Math.floor(Math.random() * countriesData.length);
-                const correctCountryData = countriesData[randomIndex];
+                const countries = countriesData.map(country => ({
+                    name: country.name.common,
+                    flag: country.flags.png
+                }));           
+
+                const randomIndex = Math.floor(Math.random() * countries.length);
+                const correctCountryData = countries[randomIndex];
+                setCorrectCountry(correctCountryData)
 
 
-                setCorrectCountry(correctCountryData);
-
-                const response = await fetch(`https://restcountries.com/v3.1/name/${correctCountryData.name.common}`);
-                const otherCountriesData = await response.json();
-                const otherCountries = otherCountriesData.filter(country => country.name.common !== correctCountryData.name.common).slice(0, 2);
-
+                const otherCountries = countries.filter(country => country.name !== correctCountryData.name).sort(() => Math.random() - 0.5).slice(0, 2);
                 const combinedOptions = [...otherCountries, correctCountryData];
                 setOptions(shuffleArray(combinedOptions));
             } catch (error) {
@@ -40,40 +37,28 @@ export const Juego = () => {
         return shuffledArray;
     };
 
-
-
     return (
         <div className={"fondo fondo_juego justify-content-center "}>
             <div
                 className={"d-flex flex-column flex-lg-row justify-content-xl-around align-items-center "}>
                 <h1 className={"titulo_juego"}>¡Hora de divertirse!</h1>
-
+            
                 <Rectangulo classNames={"z-2 mx-5 my-5"} backgroundColor={"#113946"}
                     borderColor={"#FFF2D8"}
                     textColor={"#113946"}>
                     <h1 className={"subtitulo_juego my-3 mx-5"}>Adivina la capital</h1>
-                    <img src={correctCountry.flags.png} />
+                    <img src={correctCountry.flag} />
+
 
                     <div className={"botonera_juego m-4 mt-md-5 d-flex flex-column flex-xl-row align-items-center"}>
                         {options.map((option, index) => (
-                            <button>
-                                {option.name.common}
+                            <button key={index} className={" m-4 p-3 ps-5 pe-5 boton_juego"}>
+                                {option.name}
                             </button>
                         ))}
-                        {/* <button className={" m-4 p-3 ps-5 pe-5 boton_juego"}>{correctCountry.name.common}</button>
-                        <button className={" m-4 p-3 ps-5 pe-5 boton_juego"}>Texto</button>
-                        <button className={" m-4 p-3 ps-5 pe-5 boton_juego"}>Texto</button> */}
                     </div>
                 </Rectangulo>
             </div>
         </div>
     )
 }
-
-
-
-{/* {options.map((option, index) => (
-                            <button key={index} onClick={() => handleOptionClick(option)}>
-                                {option.name.common}
-                            </button>
-                        ))} */}

597df8fa7e4854938cfdc63a2db321fb445e13fc
Victor <elsodah12@gmail.com>
2024-04-04 20:45:16 +0200

    He separado la funcionalidad del juego en un fichero js aparte, que lo tenia todo en el useEffect y no me gusta que hiciera varios fetch cada vez que se recargaba el componente y asi lo puedo utilizar en mas de un sitio para el otro modo de juego que incorporaré más adelante

--- src/componentes/Juego.jsx
+++ src/componentes/Juego.jsx
@@ -1,57 +1,33 @@
 import React, { useState, useEffect } from "react"
 import { Rectangulo } from "./Rectangulo.jsx";
+import { correctCountry, options } from '../utils/juego.js';
 
 
 export const Juego = () => {
-    const [correctCountry, setCorrectCountry] = useState({});
-    const [options, setOptions] = useState([]);
+
+    const [correctAnswer, setCorrectAnswer] = useState({});
+    const [buttonOptions, setButtonOptions] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const flagsApiResponse = await fetch('https://restcountries.com/v3.1/all');
-                const countriesData = await flagsApiResponse.json();
-
-                const countries = countriesData.map(country => ({
-                    name: country.name.common,
-                    flag: country.flags.png
-                }));           
-
-                const randomIndex = Math.floor(Math.random() * countries.length);
-                const correctCountryData = countries[randomIndex];
-                setCorrectCountry(correctCountryData)
-
-
-                const otherCountries = countries.filter(country => country.name !== correctCountryData.name).sort(() => Math.random() - 0.5).slice(0, 2);
-                const combinedOptions = [...otherCountries, correctCountryData];
-                setOptions(shuffleArray(combinedOptions));
-            } catch (error) {
-                console.error('Error con el fetch:', error);
-            }
-        };
-        fetchData();
+        const correctAnswerResult = correctCountry();
+        const buttonOptions = options(correctAnswerResult);
+        setCorrectAnswer(correctAnswerResult)
+        setButtonOptions(buttonOptions);
     }, []);
-
-    const shuffleArray = (array) => {
-        const shuffledArray = array.sort(() => Math.random() - 0.5);
-        return shuffledArray;
-    };
 
     return (
         <div className={"fondo fondo_juego justify-content-center "}>
             <div
                 className={"d-flex flex-column flex-lg-row justify-content-xl-around align-items-center "}>
                 <h1 className={"titulo_juego"}>¡Hora de divertirse!</h1>
-            
+
                 <Rectangulo classNames={"z-2 mx-5 my-5"} backgroundColor={"#113946"}
                     borderColor={"#FFF2D8"}
                     textColor={"#113946"}>
                     <h1 className={"subtitulo_juego my-3 mx-5"}>Adivina la capital</h1>
-                    <img src={correctCountry.flag} />
-
-
+                    <img src={correctAnswer.flag} />
                     <div className={"botonera_juego m-4 mt-md-5 d-flex flex-column flex-xl-row align-items-center"}>
-                        {options.map((option, index) => (
+                        {buttonOptions.map((option, index) => (
                             <button key={index} className={" m-4 p-3 ps-5 pe-5 boton_juego"}>
                                 {option.name}
                             </button>
---
+++ src/utils/juego.js
@@ -0,0 +1,42 @@
+
+var countries = [];
+
+const fetchData = async () => {
+    try {
+        const flagsApiResponse = await fetch('https://restcountries.com/v3.1/all');
+        const countriesData = await flagsApiResponse.json();
+
+        countries = countriesData.map(country => ({
+            name: country.name.common,
+            flag: country.flags.png
+        }));
+    } catch (error) {
+        console.error('Error con el fetch:', error);
+    }
+};
+
+export function correctCountry() {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    return countries[randomIndex]
+}
+
+export function options(correctCountryVar) {
+    const otherCountries = countries.filter(country => country.name !== correctCountryVar.name).sort(() => Math.random() - 0.5).slice(0, 2);
+    return shuffleArray([...otherCountries, correctCountryVar]);
+}
+
+// FUNCIONES COMUNES
+
+function shuffleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+};
+
+// const shuffleArray = array => array.sort(() => Math.random() - 0.5);
+
+fetchData();
+
+
+
+
+
+

d988274cf91b832c665a4a84fe2e7b2ce5873348
JuanValeraDev <juanvaleradev@gmail.com>
2024-04-05 21:55:30 +0200

    He avanzado en el buscador. Me falta ya poquillo

--- src/componentes/Buscador.jsx
+++ src/componentes/Buscador.jsx
@@ -1,57 +1,211 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {Rectangulo} from "./Rectangulo.jsx";
-import {Col, Row} from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 import RectanguloBuscador from "./RectanguloBuscador.jsx";
 
-export const Buscador = () => {
+export const MyContext = React.createContext();
 
+
+export const Buscador = () => {
+    const [names, setNames] = useState([]);
+    const [selectedName, setSelectedName] = useState(null);
+    const [currencies, setCurrencies] = useState([]);
+    const [selectedCurrency, setSelectedCurrency] = useState(null);
+    const [languages, setLanguages] = useState([]);
+    const [selectedLanguage, setSelectedLanguage] = useState(null);
+    const [regions, setRegions] = useState([]);
+    const [selectedRegion, setSelectedRegion] = useState(null);
+    const [subregions, setSubregions] = useState([]);
+    const [selectedSubregion, setSelectedSubregion] = useState(null);
+    const [capitals, setCapitals] = useState([]);
+    const [selectedCapital, setSelectedCapital] = useState(null);
+
+    const [results, setResults] = useState([]);
+    const [lastFetchedName, setLastFetchedName] = useState(null);
+
+
+
+    const selectData = [
+        {field: 'Nombre', options: names},
+        {field: 'Moneda', options: currencies},
+        {field: 'Idioma', options: languages},
+        {field: 'Región', options: regions},
+        {field: 'Subregión', options: subregions},
+        {field: 'Capital', options: capitals},
+    ];
+
+    const dataSearched = {
+        "Nombre": names,
+        "Moneda": currencies,
+        "Idioma": languages,
+        "Región": regions,
+        "Subregión": subregions,
+        "Capital": capitals
+    }
+
+    const dataSelected = {
+        "Nombre": selectedName,
+        "Moneda": selectedCurrency,
+        "Idioma": selectedLanguage,
+        "Región": selectedRegion,
+        "Subregión": selectedSubregion,
+        "Capital": selectedCapital
+    }
+
+    let filteredData = {};
+    for (let [key, value] of Object.entries(dataSelected)) {
+        if (value !== null) {
+            filteredData[key] = dataSearched[key].filter(option => option === value);
+        }
+    }
+
+
+
+    const handleNameChange = (selectedOption) => {
+        setSelectedName(selectedOption);
+    };
+    const handleCurrencyChange = (selectedOption) => {
+        setSelectedCurrency(selectedOption);
+    }
+    const handleLanguageChange = (selectedOption) => {
+        setSelectedLanguage(selectedOption);
+    }
+    const handleRegionChange = (selectedOption) => {
+        setSelectedRegion(selectedOption);
+    }
+    const handleSubregionChange = (selectedOption) => {
+        setSelectedSubregion(selectedOption);
+
+    }
+
+    const handleCapitalChange = (selectedOption) => {
+        setSelectedCapital(selectedOption);
+
+    }
+
+    const handlers = {
+        "Nombre": handleNameChange,
+        "Moneda": handleCurrencyChange,
+        "Idioma": handleLanguageChange,
+        "Región": handleRegionChange,
+        "Subregión": handleSubregionChange,
+        "Capital": handleCapitalChange
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+
+                const response = await fetch("https://restcountries.com/v3.1/all?fields=name,currencies,languages,region,subregion,capital");
+                const data = await response.json();
+
+                let apiUrl = "https://restcountries.com/v3.1/name/";
+                if (filteredData["Nombre"] && filteredData["Nombre"][0].value !== lastFetchedName) {
+                    apiUrl += filteredData["Nombre"][0].value;
+                    setLastFetchedName(filteredData["Nombre"][0].value);
+
+                }
+                try {
+                    const response = await fetch(apiUrl);
+                    const data = await response.json();
+                    setResults(data);
+                } catch (error) {
+                    console.error("Error al obtener los países:", error);
+                }
+                const names = data.map(country => ({value: country.name.common, label: country.name.common}));
+                const uniqueNames = getUniqueSorted(names);
+                setNames(uniqueNames);
+
+                const currencies = data.flatMap(country =>
+                    Object.values(country.currencies).map(currency => ({value: currency.name, label: currency.name}))
+                );
+                const uniqueCurrencies = getUniqueSorted(currencies);
+                setCurrencies(uniqueCurrencies);
+
+                const languages = data.flatMap(country =>
+                    Object.values(country.languages).map(language => ({value: language, label: language}))
+                );
+                const uniqueLanguages = getUniqueSorted(languages);
+                setLanguages(uniqueLanguages);
+
+                const regions = data.map(country => ({value: country.region, label: country.region}));
+                const uniqueRegions = getUniqueSorted(regions);
+                setRegions(uniqueRegions);
+
+                const subregions = data.map(country => ({value: country.subregion, label: country.subregion}));
+                const uniqueSubregions = getUniqueSorted(subregions);
+                setSubregions(uniqueSubregions);
+
+                const capitals = data.reduce((acc, country) => {
+                    if (country.capital[0]) {
+                        acc.push({value: country.capital[0], label: country.capital[0]});
+                    }
+                    return acc;
+                }, []);
+                const uniqueCapitals = getUniqueSorted(capitals);
+                setCapitals(uniqueCapitals);
+
+            } catch (error) {
+                console.error("Error al obtener los datos:", error);
+            }
+        };
+
+        const getUniqueSorted = (items) => {
+            const uniqueItems = items.reduce((acc, current) => {
+                const x = acc.find(item => item.label === current.label);
+                if (!x) {
+                    return acc.concat([current]);
+                } else {
+                    return acc;
+                }
+            }, []);
+            uniqueItems.sort((a, b) => a.label.localeCompare(b.label));
+            return uniqueItems;
+        };
+
+        fetchData();
+    }, [filteredData]);
 
     return (
-        <div className={"fondo fondo_buscador pb-5 pb-lg-0 main-content"}>
-            <div className={"d-flex flex-column"}>
-                <h1 className={"titulo_buscador pt-5 mt-lg-5 mb-5"}>Busca como un profesional</h1>
-                <Row className={"mb-lg-5  justify-content-center "}>
-                    <Col xs={11} sm={7} lg={6} xl={5} className={"z-2 justify-content-lg-start me-xl-5"}>
+        <MyContext.Provider value={{selectData, handlers}}>
 
-                        <RectanguloBuscador/>
-                    </Col>
-                    <Col xs={11} sm={7} lg={6} xl={5} className={"justify-content-lg-end"}>
+            <div className={"fondo fondo_buscador pb-5 pb-lg-0 main-content"}>
+                <div className={"d-flex flex-column"}>
+                    <h1 className={"titulo_buscador pt-5 mt-lg-5 mb-5"}>Busca como un profesional</h1>
+                    <Row className={"mb-lg-5  justify-content-center "}>
+                        <Col xs={11} sm={7} lg={6} xl={5} className={"z-2 justify-content-lg-start me-xl-5"}>
 
-                        <Rectangulo classNames={"rectangulo_buscador_2 container mt-5 mt-lg-0 ms-xl-5 p-sm-4"}
-                                    backgroundColor={"#FFF2D8"}
-                                    borderColor={"#113946"}
-                                    textColor={"#113946"} padding={{padding: "20px"}}
-                        >
-                            <h1 className={"m-4"}>Resultados</h1>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat ac ante sed
-                            accumsan.
-                            Vestibulum
-                            venenatis magna orci, eu iaculis leo interdum eget. Aliquam eget nulla neque. Vivamus
-                            tellus
-                            tellus,
-                            molestie et mauris sit amet, interdum imperdiet arcu. Pellentesque pulvinar nisl at elit
-                            sodales
-                            ultrices. Ut nec tincidunt justo. Curabitur condimentum interdum elit, in sagittis purus
-                            tempus at.
-                            Sed
-                            egestas venenatis blandit. Vestibulum ornare in erat vel consequat. Lorem ipsum dolor
-                            sit
-                            amet, consectetur adipiscing elit. In placerat ac ante sed accumsan.
-                            Vestibulum
-                            venenatis magna orci, eu iaculis leo interdum eget. Aliquam eget nulla neque. Vivamus
-                            tellus
-                            tellus,
-                            molestie et mauris sit amet, interdum imperdiet arcu. Pellentesque pulvinar nisl at elit
-                            sodales
-                            ultrices. Ut nec tincidunt justo. Curabitur condimentum interdum elit, in sagittis purus
-                            tempus at.
-                            Sed
-                            egestas venenatis blandit. Vestibulum ornare in erat vel consequat.
-                        </Rectangulo>
-                    </Col>
-                </Row>
+                            <RectanguloBuscador/>
+                        </Col>
+                        <Col xs={11} sm={7} lg={6} xl={5} className={"justify-content-lg-end"}>
+
+                            <Rectangulo classNames={"rectangulo_buscador_2 container mt-5 mt-lg-0 ms-xl-5 p-sm-4"}
+                                        backgroundColor={"#FFF2D8"}
+                                        borderColor={"#113946"}
+                                        textColor={"#113946"} padding={{padding: "20px"}}
+                            >
+                                <h1 className={"m-4"}>Resultados</h1>
+                                <div className={"container"}>
+                                    <Row>
+                                        {Object.entries(results).map(([field, options], index) => (
+                                            <Col xs={12} sm={6} lg={4} key={index}>
+                                                <Card key={index} field={field} option={options}>
+                                                    <Card.Body>
+                                                        <Card.Title>{field}</Card.Title>
+                                                        <Card.Text>{options.label}</Card.Text>
+                                                    </Card.Body>
+                                                </Card>
+                                            </Col>
+                                        ))}
+                                    </Row>
+                                </div>
+                            </Rectangulo>
+                        </Col>
+                    </Row>
+                </div>
             </div>
-        </div>
+        </MyContext.Provider>
+
     )
 
 }
--- src/componentes/RectanguloBuscador.jsx
+++ src/componentes/RectanguloBuscador.jsx
@@ -1,165 +1,28 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Row, Col} from "react-bootstrap";
 import Select from "react-select";
 import {countries} from '../utils/countries.js'
+import {MyContext} from "./Buscador.jsx";
 
+
+/*
+En el rectángulo del resultado puedo poner los resultados en forma de tarjetas con la imagen de la bandera del país y el nombre del país.
+Para ello, puedo hacer un fetch a la API de restcountries y obtener los nombres y las banderas de los países.
+Luego, puedo mostrar los resultados en forma de tarjetas.
+
+Para las tarjetas, puedo usar react-bootstrap y hacer un componente Card que reciba los detalles del país y los muestre.
+Cada tarjeta se puede abrir al hacer click en ella y mostrar más detalles del país en un modal.
+
+Para el modal, puedo usar react-bootstrap y hacer un componente Modal que reciba los detalles del país y los muestre.
+
+
+
+
+ */
 
 const RectanguloBuscador = () => {
 
-    const fields = ["Nombre", "Moneda", "Idioma", "Región", "Subregión", "Gentilicio"];
-    const [names, setNames] = useState([]);
-    const [currencies, setCurrencies] = useState([]);
-    const [languages, setLanguages] = useState([]);
-    const [regions, setRegions] = useState([]);
-    const [subregions, setSubregions] = useState([]);
-    const [capitals, setCapitals] = useState([]);
-
-    const selectData = [
-        {field: 'Nombre', options: names},
-        {field: 'Moneda', options: currencies},
-        {field: 'Idioma', options: languages},
-        {field: 'Región', options: regions},
-        {field: 'Subregión', options: subregions},
-        {field: 'Capital', options: capitals},
-    ];
-
-
-    useEffect(() => {
-        const fetchNames = async () => {
-            try {
-                const response = await fetch("https://restcountries.com/v3.1/all?fields=name");
-                const data = await response.json();
-                const commonNames = data.map(country => ({value: country.name.common, label: country.name.common}));
-                const uniqueNames = commonNames.reduce((acc, current) => {
-                    const x = acc.find(item => item.label === current.label);
-                    if (!x) {
-                        return acc.concat([current]);
-                    } else {
-                        return acc;
-                    }
-                }, []);
-                uniqueNames.sort((a, b) => a.label.localeCompare(b.label));
-                setNames(uniqueNames);
-            } catch (error) {
-                console.error("Error al obtener los países:", error);
-            }
-        };
-        const fetchCurrency = async () => {
-            try {
-                const response = await fetch("https://restcountries.com/v3.1/all?fields=currencies");
-                const data = await response.json();
-                const currencyNames = data.flatMap(country =>
-                    Object.values(country.currencies).map(currency => ({value: currency.name, label: currency.name}))
-                );
-                const uniqueNames = currencyNames.reduce((acc, current) => {
-                    const x = acc.find(item => item.label === current.label);
-                    if (!x) {
-                        return acc.concat([current]);
-                    } else {
-                        return acc;
-                    }
-                }, []);
-                uniqueNames.sort((a, b) => a.label.localeCompare(b.label));
-                setCurrencies(uniqueNames);
-            } catch (error) {
-                console.error("Error al obtener las monedas:", error);
-            }
-        };
-        const fetchLanguage = async () => {
-            try {
-                const response = await fetch("https://restcountries.com/v3.1/all?fields=languages");
-                const data = await response.json();
-                const languageNames = data.flatMap(country =>
-                    Object.values(country.languages).map(language => ({value: language, label: language}))
-                );
-                const uniqueLanguageNames = languageNames.reduce((acc, current) => {
-                    const x = acc.find(item => item.label === current.label);
-                    if (!x) {
-                        return acc.concat([current]);
-                    } else {
-                        return acc;
-                    }
-                }, []);
-                uniqueLanguageNames.sort((a, b) => a.label.localeCompare(b.label));
-                setLanguages(uniqueLanguageNames);
-            } catch (error) {
-                console.error("Error al obtener los idiomas:", error);
-            }
-        };
-
-        const fetchRegion = async () => {
-            try {
-                const response = await fetch("https://restcountries.com/v3.1/all?fields=region");
-                const data = await response.json();
-                const regionNames = data.map(country => ({value: country.region, label: country.region}));
-                const uniqueNames = regionNames.reduce((acc, current) => {
-                    const x = acc.find(item => item.label === current.label);
-                    if (!x) {
-                        return acc.concat([current]);
-                    } else {
-                        return acc;
-                    }
-                }, []);
-                uniqueNames.sort((a, b) => a.label.localeCompare(b.label));
-                setRegions(uniqueNames);
-
-            } catch (error) {
-                console.error("Error al obtener las regiones:", error);
-            }
-
-        };
-        const fetchSubregion = async () => {
-            try {
-                const response = await fetch("https://restcountries.com/v3.1/all?fields=subregion");
-                const data = await response.json();
-                const subregionNames = data.map(country => ({value: country.subregion, label: country.subregion}));
-                const uniqueNames = subregionNames.reduce((acc, current) => {
-                    const x = acc.find(item => item.label === current.label);
-                    if (!x) {
-                        return acc.concat([current]);
-                    } else {
-                        return acc;
-                    }
-                }, []);
-                uniqueNames.sort((a, b) => a.label.localeCompare(b.label));
-                setSubregions(uniqueNames);
-            } catch (error) {
-                console.error("Error al obtener las subregiones:", error);
-            }
-
-        };
-        const fetchCapitals = async () => {
-            try {
-                const response = await fetch("https://restcountries.com/v3.1/all?fields=capital");
-                const data = await response.json();
-                const capitalNames = data.reduce((acc, country) => {
-                    if (country.capital[0]) {
-                        acc.push({value: country.capital[0], label: country.capital[0]});
-                    }
-                    return acc;
-                }, []);
-                const uniqueNames = capitalNames.reduce((acc, current) => {
-                    const x = acc.find(item => item.label === current.label);
-                    if (!x) {
-                        return acc.concat([current]);
-                    } else {
-                        return acc;
-                    }
-                }, []);
-                uniqueNames.sort((a, b) => a.label.localeCompare(b.label));
-                setCapitals(uniqueNames);
-            } catch (error) {
-                console.error("Error al obtener las capitales:", error);
-            }
-        };
-        fetchLanguage();
-        fetchRegion();
-        fetchSubregion();
-        fetchCapitals();
-        fetchNames();
-        fetchCurrency();
-    }, []);
-
+   const {handlers, selectData} = useContext(MyContext);
 
     return (
         <div style={{
@@ -188,6 +51,7 @@ const RectanguloBuscador = () => {
                                         options={select.options}
                                         isClearable
                                         isSearchable
+                                        onChange={handlers[select.field]}
                                     />
                                 </Col>
                             </Row>
@@ -201,59 +65,6 @@ const RectanguloBuscador = () => {
 
 export default RectanguloBuscador;
 
-{    /*
-const filas1 = ["Nombre:", "Moneda:", "Idioma:"];
-const filas2 = ["Región:", "Subreg:", "Código:"];
 
-return (
-<div style={{
-    backgroundColor: "#113946",
-    borderColor: "#FDF6EA",
-    color: "#FDF6EA",
-    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.6)",
-    overflow: "hidden",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "50px",
-    border: "thick solid"
-}}>
-    <Row className="d-flex align-items-center justify-content-center mb-2 p-2">
-        <Col xs={12} lg={6}>
-            {filas1.map((texto, index) => (
-                <Row key={index} className="d-flex align-items-center justify-content-center mb-2 p-3 ">
-                    <Col className="text-center ms-lg-3 me-lg-3">{texto}</Col>
-                    <Col>
-                        <input
-                            className="buscador_input"
-                            type="text"
-                            maxLength={10}
-                            style={{backgroundColor: '#FDF6EA'}}
-                        />
-                    </Col>
-                </Row>
-            ))}
-        </Col>
-        <Col xs={12} lg={6}>
-            {filas2.map((texto, index) => (
-                <Row key={index} className="d-flex align-items-center justify-content-center mb-2 p-3">
-                    <Col className="text-center ms-lg-3 me-lg-3">{texto}</Col>
-                    <Col className={"me-2"}>
-                        <input
-                            className="buscador_input"
-                            type="text"
-                            maxLength={10}
-                            style={{backgroundColor: '#FDF6EA'}}
-                        />
-                    </Col>
-                </Row>
-            ))}
-        </Col>
-    </Row>
-</div>
-);
-*/
-}
 
 

3485bc5c5ca4ce5011172efb4e58a4ce9f134779
Victor <elsodah12@gmail.com>
2024-04-06 14:52:26 +0200

    Primera ventana diseñada

--- src/componentes/BotonNivel.jsx
+++ src/componentes/BotonNivel.jsx
@@ -1,9 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import Popup from 'reactjs-popup';
+import { FaQuestionCircle } from 'react-icons/fa';
+
+// export const BotonNivel = (props) => {
+//     return (
+//         <div className="align-items-center text-center p-2">
+//             <button className="boton_seleccion_nivel">{props.name}
+//                 <Popup trigger={<button className="buttonPopUp"><FaQuestionCircle className="icono_interrogacion"/></button>}
+//                     position="right center">
+//                     <div className="game_desc">{props.desc}</div>
+//                 </Popup>
+//             </button>
+//         </div>
+//     )
+// }
 
 export const BotonNivel = (props) => {
+    const [popupPosition, setPopupPosition] = useState("right center");
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 768) {
+                setPopupPosition("bottom center");
+            } else {
+                setPopupPosition("right center");
+            }
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <div className="align-items-center text-center p-2">
-            <button className="boton_seleccion_nivel">{props.name}</button>
+            <button className="boton_seleccion_nivel">{props.name}
+                <Popup trigger={<button className="buttonPopUp"><FaQuestionCircle className="icono_interrogacion"/></button>}
+                    position={popupPosition}>
+                    <div className="game_desc">{props.desc}</div>
+                </Popup>
+            </button>
         </div>
     )
 }
--- src/componentes/SeleccionNivel.jsx
+++ src/componentes/SeleccionNivel.jsx
@@ -1,24 +1,15 @@
 import React, { useState } from "react"
 import { BotonNivel } from "./BotonNivel"
-import { FaQuestionCircle } from 'react-icons/fa';
 
 export const SeleccionNivel = () => {
-    const [showPopup, setShowPopup] = useState(false);
-
-
     return (
         <div className="fondo fondo_juego d-flex flex-column align-items-center justify-content-center">
             <h1 className="titulo_juego">¡Hora de divertirse!</h1>
             <h3 className="descripcion_juego">¡Pon a prueba tus conocimientos sobre los distintos países!</h3>
-            <FaQuestionCircle className="icono_interrogacion" onClick={() => setShowPopup(true)} />
-            {showPopup && (
-                <div className="popup">
-                    <p>Texto de ayuda o descripción sobre el juego...</p>
-                    <button onClick={() => setShowPopup(false)}>Cerrar</button>
-                </div>
-            )}
-            <BotonNivel name="Adivina la capital"/>
-            <BotonNivel name="Adivina la bandera" />
+            <div className="my-5">
+                <BotonNivel name="Adivina la capital" desc="Pon a prueba tus conocimientos para adivinar las capitales de diferentes países. ¿Estás listo para aceptar el desafío y convertirte en un maestro de la geografía mundial?" />
+                <BotonNivel name="Adivina la bandera" desc="Se te presenta una bandera y debes adivinar el país al que pertenece. ¿Serás capaz de adivinar todas las banderas?" />
+            </div>
         </div>
     )
 }

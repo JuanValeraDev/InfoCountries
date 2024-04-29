c0262f53b570d0927e6e62474cd76290bed1fa71
Victor <elsodah12@gmail.com>
2024-04-29 17:25:17 +0200

    Beta 0.0.0.0.0.1 V12.0.2.tusmuertos de el comparador

--- src/componentes/Comparador.jsx
+++ src/componentes/Comparador.jsx
@@ -6,11 +6,13 @@ import { countries, theme } from '../utils/countries.js'
 import Slider from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { testText } from "../utils/temp.js";
 
 
 export const Comparador = () => {
 
     const [selectedOptions, setSelectedOptions] = useState([]);
+    const [result, setResult] = useState([])
 
     const settings = {
         dots: true,
@@ -19,9 +21,11 @@ export const Comparador = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: false,
-        autoplaySpeed: 2000, // adjust autoplay speed as needed
     };
 
+    const compare = () => {
+        setResult(testText)
+    }
 
 
     return <div className={"fondo fondo_comparador"}>
@@ -38,53 +42,60 @@ export const Comparador = () => {
                         isSearchable
                         placeholder={"Paises a comparar (Máximo 3 países)"} />
                     <Select className="mt-3 buscador-galeria text-start buscadores" options={theme} isClearable isSearchable placeholder={"temática a comparar"} />
-                    <button className="my-3">Comparar</button>
+                    <button onClick={compare} className="my-3 boton_comparar">Comparar</button>
                 </Col>
 
 
             </div>
-            <div className="d-flex flex-column flex-lg-row justify-content-evenly align-items-center">
-
+            <div className="d-flex flex-column align-items-center">
                 <Col xs={12} sm={10} lg={5} xxl={5}>
-                    <Slider {...settings}>
-                        <div>
-                            <h3>Slide 1</h3>
-                            <p>Text for slide 1</p>
-                        </div>
-                        <div>
-                            <h3>Slide 2</h3>
-                            <p>Text for slide 2</p>
-                        </div>
-                        <div>
-                            <h3>Slide 3</h3>
-                            <p>Text for slide 3</p>
-                        </div>
-                        {/* Add more slides as needed */}
-                    </Slider>
+                    {result.length > 0 && (<Slider {...settings}>
 
+                        {result.map((text, index) => (
+                            <Rectangulo classNames={"my-5"}
+                                backgroundColor={"#113946"}
+                                borderColor={"#FDF6EA"}
+                                textColor={"#FDF6EA"}
+                                height={{ height: "min-content" }}
+                                width={{ width: "auto" }}>
+                                <p className="p-2 p-sm-4 text-center">{text}</p>
+                            </Rectangulo>
+                        ))}
+                    </Slider>)}
                     {/* <Rectangulo classNames={"my-5"}
                             backgroundColor={"#113946"}
                             borderColor={"#FDF6EA"}
                             textColor={"#FDF6EA"}
-                            height={{height: "min-content"}}
-                            width={{width: "auto"}}>
-                    <text className="p-2 p-sm-4 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint esse beatae
-                        obcaecati voluptate impedit fuga, cupiditate est animi temporibus molestias architecto totam
-                        provident atque optio nesciunt doloribus reprehenderit quis eligendi?
-                        Voluptate sequi illum facilis quod ratione atque velit. Doloremque quo quam explicabo esse vel
-                        dolore, autem, excepturi illum ea et dolorum mollitia. Rem adipisci ipsum aliquid amet, ea
-                        sapiente
-                        perferendis.
-                        Asperiores, delectus ullam magni consequuntur quas ut hic necessitatibus rerum, alias facere,
-                        odio
-                        atque porro voluptates accusamus pariatur earum nobis dolores cumque voluptatem nostrum numquam
-                        eveniet doloribus reprehenderit laboriosam. Culpa.
-                    </text>
-                </Rectangulo> */}
+                            height={{ height: "min-content" }}
+                            width={{ width: "auto" }}>
+                            <p className="p-2 p-sm-4 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint esse beatae
+                                obcaecati voluptate impedit fuga, cupiditate est animi temporibus molestias architecto totam
+                                provident atque optio nesciunt doloribus reprehenderit quis eligendi?
+                                Voluptate sequi illum facilis quod ratione atque velit. Doloremque quo quam explicabo esse ve
+                            </p>
+                        </Rectangulo>
+                        <Rectangulo classNames={"my-5"}
+                            backgroundColor={"#113946"}
+                            borderColor={"#FDF6EA"}
+                            textColor={"#FDF6EA"}
+                            height={{ height: "min-content" }}
+                            width={{ width: "auto" }}>
+                            <p className="p-2 p-sm-4 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint esse beatae
+                                obcaecati voluptate impedit fuga, cupiditate est animi temporibus molestias architecto totam
+                                provident atque optio nesciunt doloribus reprehenderit quis eligendi?
+                                Voluptate sequi illum facilis quod ratione atque velit. Doloremque quo quam explicabo esse ve
+                            </p>
+                        </Rectangulo> */}
+
                 </Col>
+            </div>
+        </div>
+    </div>
+}
 
 
-                {/* <Col xs={12} sm={10} lg={5} xxl={5}>
+
+{/* <Col xs={12} sm={10} lg={5} xxl={5}>
                 <Rectangulo classNames={"my-5"}
                             backgroundColor={"#113946"}
                             borderColor={"#FDF6EA"}
@@ -105,7 +116,3 @@ export const Comparador = () => {
                     </text>
                 </Rectangulo>
                 </Col> */}
-            </div>
-        </div>
-    </div>
-}

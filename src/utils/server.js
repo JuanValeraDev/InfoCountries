dc1bea0894f13528bc839465474029569c34387a
JuanValeraDev <juanvaleradev@gmail.com>
2024-05-13 19:36:05 +0200

    He modificado server.js y ya no da el error que daba con __dirname. Por lo menos en local debería funcionar, en render no estoy seguro

--- src/utils/server.js
+++ src/utils/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import path from 'path';
 import cors from "cors";
 import OpenAI from "openai";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Sirve los archivos estáticos de tu aplicación (por ejemplo, CSS, JS, imágenes)
 const publicPath = path.join(__dirname, '..', 'dist');

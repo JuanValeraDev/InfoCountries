1ed8116cafcf6d934f98ae43780ed835f626add0
JuanValeraDev <juanvaleradev@gmail.com>
2024-05-28 21:09:37 +0200

    He modificado la forma en la que se resuelve la variable publicPath. Creo que el problema viene de que en package.json, para lanzar el servicio de express el comando es este: concurrently 'vite' 'node src/utils/server.js' y eso hace que a la hora de sacar la ruta del archivo empiece desde ahí. Es decir, está creyendo que el directorio raíz es la ruta absoluta de server.js.

--- src/utils/server.js
+++ src/utils/server.js
@@ -14,10 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve(path.dirname(__filename), '..');
 
-// Sirve los archivos estáticos de tu aplicación (por ejemplo, CSS, JS, imágenes)
-//const publicPath = path.join(__dirname, '..', 'dist');
 const publicPath = path.join(__dirname, 'dist');
 
 console.log("__dirname", __dirname)
